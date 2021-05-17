4dab1db7e9c8317b607ad8b8fcfffb3fbc55ebb4
Ashutosh Bhargava <ashutumho@gmail.com>
2021-05-18 01:28:35 +0530

    Hiding username and password in config

--- SQL-Connections/listDBName.py
+++ SQL-Connections/listDBName.py
@@ -1,12 +1,15 @@
+# showing the list of database
+# Hiding user name and password
 from getpass import getpass
+import config
 from mysql.connector import connect,Error
 
 #Making connection with SQL
 try:
     with connect(
         host="localhost",
-        user=input("User Name: "),
-        password = getpass("your password: "),
+        user=config.username,
+        password =config.password,
         ) as connection:
         show_db_query = "SHOW DATABASES"
         with connection.cursor() as cursor:
